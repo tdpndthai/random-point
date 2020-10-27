799f354e8c40e98e57d0fb7b0b4ef943b49c7b26
Nguyễn Danh Thái <47393239+tdpndthai@users.noreply.github.com>
2020-10-27 22:00:34 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <div>
+      <img class="whcus" src="@/assets/like.jpg" alt="" />
+    </div>
+    <HelloWorld />
+    <div>
+      Copyright (c) {{ date }}
+      <b-link href="https://www.facebook.com/thainguyen.mta/" target="blank"
+        >Nguyễn Danh Thái</b-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      date: 0,
+    };
+  },
+  created() {
+    let n = new Date();
+    let y = n.getFullYear();
+    let m = n.getMonth() + 1;
+    let d = n.getDate();
+    this.date = d + "/" + m + "/" + y;
+  },
+};
 </script>
 
 <style>
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +44,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.copyright {
+  position: relative;
+}
+
+.whcus {
+  width: 500px;
+  height: 400px;
 }
 </style>
